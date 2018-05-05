b36dadbddc9d464d04fcda774ff25f2f637a7c49
Grace Miller <gracemiller@Graces-MacBook-Pro-2.local>
2018-05-05 11:32:38 -0500

    added js functions to pull information submitted through form, clear form, send data to database, pull data from database and write into new row on table

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,57 @@
     };
     firebase.initializeApp(config);
 
+    var database = firebase.database();
+
+//Pull info from form on submission and store in database
+
+    $("#submit").on("click", function(){
+        event.preventDefault();
+
+        var trainName = $("#train-name-input").val().trim();
+        var trainDestination = $("#train-destination-input").val().trim();
+        var firstTrainTime = $("#train-start-time-input").val().trim();
+        var trainFrequency = $("#train-frequency-input").val().trim();
+
+        console.log(trainName, trainDestination, firstTrainTime, trainFrequency);
+
+        $("#train-name-input").val("");
+        $("#train-destination-input").val("");
+        $("#train-start-time-input").val("");
+        $("#train-frequency-input").val("");
+
+        var newTrain = {
+            Name: trainName,
+            Destination: trainDestination,
+            Start: firstTrainTime,
+            Frequency: trainFrequency
+        }
+
+        database.ref().push(newTrain);
+
+        alert("Train successfully added!");
+
+    });
+
+//Access train information in the database and add it as a new row to the table
+
+    database.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+        var tName = childSnapshot.val().Name;
+        var tDestination = childSnapshot.val().Destination;
+        var firstTime = childSnapshot.val().Start;
+        var tFrequency = childSnapshot.val().Frequency;
+
+        console.log(tName, tDestination, firstTime, tFrequency);
+
+        //Calculate next arrival and minutes away
+        var nextArrival = "Placeholder";
+        var minutesAway = "Placeholder";
+
+
+        $("#train-schedule").append(
+            "<tr> <td>" + tName + "</td>" + "<td>" + tDestination + "</td>" + "<td>" + tFrequency + "</td>" +
+            "<td>" + nextArrival + "</td>" + "<td>" + minutesAway + "</td> </tr>"
+        )
+
+    });
