d8f0c9cfcdabcdb5aee0a96b743ed892f7c148a3
Grace Miller <gracemiller@Graces-MacBook-Pro-2.local>
2018-05-05 09:53:05 -0500

    set up index.html;  added cdns for bootstrap, moment.js, jquery; linked stylesheets; configured firebase

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA8jJe3S04eH17mzA1WPKc8VM3Fz_ZkVyE",
+        authDomain: "train-time-tracker-6dd9e.firebaseapp.com",
+        databaseURL: "https://train-time-tracker-6dd9e.firebaseio.com",
+        projectId: "train-time-tracker-6dd9e",
+        storageBucket: "",
+        messagingSenderId: "571618491750"
+    };
+    firebase.initializeApp(config);
+
